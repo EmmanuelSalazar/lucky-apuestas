db34619b2a48c97fd9b73fd16378a2620d8ef376
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2024-07-26 17:03:19 -0500

    BETTER ARCHITECTURE

--- Untitled-3.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('generate-btn').addEventListener('click', function() {
-    const number = Math.floor(1000 + Math.random() * 9000);
-    document.getElementById('number').textContent = number;
-});
