eba92a2b8292f6ba1183efbffcca637515342815
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2024-07-27 15:03:42 -0500

    V1.0

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() { 
+    $('#checbox1').click(function(){
+        $('#checkboxLabel').css('background', '#388C1A');
+        $('#checkboxLabel').attr('checked', '1');
+        console.log($('#checbox1').css('background', '#388C1A'));
+        }
+    )
+
+
+})
---
+++ src/js/random.js
@@ -0,0 +1,4 @@
+document.getElementById('genNum').addEventListener('click', function() {
+    const number = Math.floor(1000 + Math.random() * 9000);
+    document.getElementById('number').textContent = number;
+});
