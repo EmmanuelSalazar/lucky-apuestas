4b253842aee736092f6eab31254546758cad9c69
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2024-07-28 09:18:20 -0500

    BETTER CODE

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,34 @@
 $(document).ready(function() { 
-    $('#checbox1').click(function(){
-        $('#checkboxLabel').css('background', '#388C1A');
-        $('#checkboxLabel').attr('checked', '1');
-        console.log($('#checbox1').css('background', '#388C1A'));
+    const fechaActual = new Date();
+    const mes = fechaActual.getMonth();
+    const año = fechaActual.getFullYear();
+    const fechaFormateada = `${mes + 1}/${año}`;
+    $('#checkboxLabel1').append(fechaFormateada);
+    /* $('input').on("click", function(){
+        var inputRadio = parseInt($('input:checked').val(), 10);           
+            switch (inputRadio) {
+                case 1: 
+                        $('#checkboxLabel1').css('background', '#388C1A');
+                        console.log(inputRadio);
+                    break;
+                case 2:
+                        $('#checkboxLabel2').css('background', '#388C1A');
+                        console.log(inputRadio);
+                    break;
+                case 3:
+                        $('#checkboxLabel3').css('background', '#388C1A');
+                        console.log(inputRadio);
+                    break; 
+                case 4:
+                        $('#checkboxLabel4').css('background', '#388C1A');
+                        console.log(inputRadio);
+                    break;   
+
+                default:
+                    break;
+            }   
         }
-    )
+    ) */
 
 
 })
