214d1c77df270a2d36295567b494dd2cc7a2a6c0
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2024-07-28 09:56:52 -0500

    clean code

--- src/js/index.js
+++ src/js/index.js
@@ -4,31 +4,4 @@ $(document).ready(function() {
     const año = fechaActual.getFullYear();
     const fechaFormateada = `${mes + 1}/${año}`;
     $('#checkboxLabel1').append(fechaFormateada);
-    /* $('input').on("click", function(){
-        var inputRadio = parseInt($('input:checked').val(), 10);           
-            switch (inputRadio) {
-                case 1: 
-                        $('#checkboxLabel1').css('background', '#388C1A');
-                        console.log(inputRadio);
-                    break;
-                case 2:
-                        $('#checkboxLabel2').css('background', '#388C1A');
-                        console.log(inputRadio);
-                    break;
-                case 3:
-                        $('#checkboxLabel3').css('background', '#388C1A');
-                        console.log(inputRadio);
-                    break; 
-                case 4:
-                        $('#checkboxLabel4').css('background', '#388C1A');
-                        console.log(inputRadio);
-                    break;   
-
-                default:
-                    break;
-            }   
-        }
-    ) */
-
-
 })
