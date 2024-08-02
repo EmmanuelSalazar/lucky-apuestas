f44d8fedd194171311794120a17c71b3679fe01b
= <MrFarenhate0104@gmail.com>
2024-08-02 09:50:00 -0500

    DB REPLACE

--- src/php/db/db.php
+++ src/php/db/db.php
@@ -1,6 +1,6 @@
 <?php 
 
-$mysqli = new mysqli("localhost", "root", "", "luckydb");
+$mysqli = new mysqli("193.203.175.126", "u252950203_admin", "=Z|=kbLC[n3", "u252950203_luckydb");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
