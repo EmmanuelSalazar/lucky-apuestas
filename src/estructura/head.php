6d3c7e12b67fe4512d98101edf33a6b21e1c8b56
= <MrFarenhate0104@gmail.com>
2024-08-04 12:22:43 -0500

    ADSENSE ADDED

--- src/estructura/head.php
+++ src/estructura/head.php
@@ -3,3 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lucky Apuestas</title>
     <link rel="stylesheet" href="src/css/style.css">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5255982229126989"
+     crossorigin="anonymous"></script>
